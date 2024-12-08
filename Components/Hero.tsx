a74e35bb13bca626b987864b030c6b80740c8b12
Bilal Arslan <bilalarslan11@gmail.com>
2024-12-08 14:18:58 +0100

    New deploy

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
                 <div>
                     <h1 className="text-[35px] md-text-[50px] text-white font-bold">
-                        HI, I'M <span className="text-yellow-400">BILAL!</span>
+                        HI, I AM <span className="text-yellow-400">BILAL!</span>
                     </h1>
                     <TextEffect />
                     <p className="mt-[2rem] text-[20px] text-[#ffffff92]">
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enables static site export
+  basePath: '/Bilalarslan11/Bilalarslan11.github.io', // Replace with your GitHub repository name
+  assetPrefix: '/Bilalarslan11/Bilalarslan11.github.io', // Same as basePath
+  images: {
+    unoptimized: true,
+  }, //
   reactStrictMode: true,
 };
 
