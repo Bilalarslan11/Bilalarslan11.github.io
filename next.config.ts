be71024a5e46ab3790c4ca3d5ae5014bb465851e
Bilal Arslan <bilalarslan11@gmail.com>
2024-12-08 15:01:23 +0100

    New deploy 5

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  reactStrictMode: true,
   basePath: '/bilalarslan11.github.io',
-  trailingSlash: true, 
+  trailingSlash: true,
   images: {
     unoptimized: true,
-  }, //
-  reactStrictMode: true,
+  },
 };
 
 export default nextConfig;
