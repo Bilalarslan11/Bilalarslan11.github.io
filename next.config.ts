bc690983ef7f70caf92a53e60c7fcff3f0db96af
Bilal Arslan <bilalarslan11@gmail.com>
2024-12-08 14:47:32 +0100

    New deploy 2

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enables static site export
-  basePath: '/Bilalarslan11/Bilalarslan11.github.io', // Replace with your GitHub repository name
-  assetPrefix: '/Bilalarslan11/Bilalarslan11.github.io', // Same as basePath
   images: {
     unoptimized: true,
   }, //
