9a1686a5611d812f355674fd5186b298750bed0a
Bilal Arslan <bilalarslan11@gmail.com>
2025-06-13 13:46:08 +0200

    Logo

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -10,6 +10,13 @@ const Nav = ({ openNav, scrollToSection }: Props) => {
     return (
         <div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-theme-primary shadow-md">
             <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+                <div className="w-[4rem] h-[4rem] bg-theme-secondary rounded-full flex items-center justify-center">
+                    <img
+                        src="/images/lionC.jpg"
+                        alt="Lion Logo"
+                        className="w-[3.2rem] h-[3.2rem] object-contain"
+                    />
+                </div>
                 <h1 className="flex-[0.6] cursor-pointer text-[25px] text-theme-text font-bold">
                     ZE
                     <span className="text-theme-secondary">HAI</span>
@@ -44,9 +51,7 @@ const Nav = ({ openNav, scrollToSection }: Props) => {
                 >
                     CONTACT
                 </div>
-                <div onClick={openNav}>
-                    <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-theme-secondary" />
-                </div>
+                <div onClick={openNav}></div>
             </div>
         </div>
     );
