d9eb51aa82923a9e699a31021e1703c64858369b
Bilal Arslan <bilalarslan11@gmail.com>
2025-07-17 12:38:53 +0200

    Split css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import "@/styles/base.css";
+import "@/styles/navigation.css";
+import "@/styles/components.css";
+import "@/styles/game-cards.css";
+import "@/styles/utilities.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
