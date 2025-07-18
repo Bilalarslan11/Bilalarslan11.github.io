3b0bbbdebb8dc9458b5b34c3c260d62a1993c48a
Bilal Arslan <bilalarslan11@gmail.com>
2025-07-18 16:03:15 +0200

    Fixed 3 columsn

--- Components/GameCard.tsx
+++ Components/GameCard.tsx
@@ -89,7 +89,6 @@ const GameCard = ({ game, gameStatuses, onStatusUpdate }: Props) => {
                     className="rank-number-span"
                     style={{
                         position: "absolute",
-                        top: "50%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
                         fontSize: "16px",
